cebb2dc21224a7f4629fd0ad4bbf3fd21b575fbd
Chris Larsen <clarsen@yahoo-inc.com>
2018-12-31 16:05:35 -0800

    Change to The Apache 2.0 license.

--- src/main/java/net/opentsdb/tsd/KafkaRpcPluginThread.java
+++ src/main/java/net/opentsdb/tsd/KafkaRpcPluginThread.java
@@ -1,25 +1,25 @@
 // This file is part of OpenTSDB.
-// Copyright (C) 2017  The OpenTSDB Authors.
+// Copyright (C) 2018  The OpenTSDB Authors.
 //
-// This program is free software: you can redistribute it and/or modify it
-// under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 2.1 of the License, or (at your
-// option) any later version.  This program is distributed in the hope that it
-// will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
-// General Public License for more details.  You should have received a copy
-// of the GNU Lesser General Public License along with this program.  If not,
-// see <http://www.gnu.org/licenses/>.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package net.opentsdb.tsd;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.util.concurrent.AtomicDouble;
 import com.google.common.util.concurrent.RateLimiter;
 
 import joptsimple.internal.Strings;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -41,7 +41,6 @@ import net.opentsdb.core.TSDB;
 import net.opentsdb.data.TypedIncomingData;
 import net.opentsdb.data.deserializers.Deserializer;
 import net.opentsdb.tsd.KafkaRpcPluginGroup.TsdbConsumerType;
-import net.opentsdb.utils.JSON;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
